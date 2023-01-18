be93b824e19933a1b583e4123b494d8090328f52
Paul Bulman <paulbulman@gmail.com>
2023-01-18 09:33:55 +0000

    Remove hero post component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
+import AllPosts from "../components/all-posts";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
 import { getAllPosts } from "../lib/api";
@@ -13,8 +12,6 @@ type Props = {
 };
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
@@ -23,16 +20,7 @@ export default function Index({ allPosts }: Props) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <AllPosts posts={allPosts} />}
         </Container>
       </Layout>
     </>
