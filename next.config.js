9b4d6648c675244ef4b30cd1aceb12a94dc92c53
Paul Bulman <paulbulman@gmail.com>
2023-01-30 11:51:30 +0000

    Configure response headers

---
+++ next.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()",
+          },
+          {
+            key: "X-Xss-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+};
